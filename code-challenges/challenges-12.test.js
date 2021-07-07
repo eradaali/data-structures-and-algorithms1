037371d987805f6c519659c290cb8d14ffc4bf50
eradaali <erada.abdalrhman@gmail.com>
2021-07-07 22:11:07 +0300

    Done ch 12

--- code-challenges/challenges-12.test.js
+++ code-challenges/challenges-12.test.js
@@ -7,7 +7,12 @@ Write a function named replaceZeros that, given a string, uses Regex to replace 
 ------------------------------------------------------------------------------------------------ */
 
 const replaceZeros = (string) => {
-  // Solution code here...
+  let value = /0/g;
+
+    let newArray = string.replace(value, "zero");
+    
+    return newArray;
+
 }
 
 /* ------------------------------------------------------------------------------------------------
@@ -19,8 +24,11 @@ If the PIN is four numerical digits long, return true. Otherwise, return false.
 ------------------------------------------------------------------------------------------------ */
 
 const validatePin = (pin) => {
-  let newArray=[];
+  let value = /^[0-9]{4}$/;
 
+  let result = value.test(pin);
+  
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -31,8 +39,13 @@ Write a function named validateWord that uses a regular expression pattern to va
 If the word is between 5 and 10 characters long, return true. Otherwise, return false.
 ------------------------------------------------------------------------------------------------ */
 
-const validateWord = (word) => {
-  // Solution code here...
+const validateWord = (word) => 
+{
+  let value = /^[a-z]{5,10}$/i;
+
+    let result = value.test(word);
+
+    return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -43,8 +56,13 @@ Write a function named hasNumber that uses a regular expression pattern to deter
 If it does, return true. If not, return false.
 ------------------------------------------------------------------------------------------------ */
 
+
 const hasNumber = (string) => {
-  // Solution code here...
+  
+  let value = /[a-z]+[0-9]+/gi;
+  let result = value.test(string);
+
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -63,8 +81,12 @@ Return either true or false.
 Note: if you ever need to validate an email using a regex in practice, the Internet has the actual regex you should use. It's many many lines long.
 ------------------------------------------------------------------------------------------------ */
 
-const validateEmail = (email) => {
-  // Solution code here...
+const validateEmail = (email) => 
+{
+  
+  let value = /^\w+.\w+@([\w]+\.){1}(com|net|org)$/;
+  let result = value.test(email);
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -88,9 +110,13 @@ Your function should include a single regular expression pattern that matches an
 Return either true or false.
 ------------------------------------------------------------------------------------------------ */
 
-const validatePhoneNumber = (phoneNumber) => {
-  // Solution code here...
+const validatePhoneNumber = (phoneNumber) => 
+{
+  let value = /^((\(\d{3}\))|\d{3})(\s|\-)?\d{3}(\s|\-)?\d{4}$/;
+
+    return value.test(phoneNumber);
 };
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
